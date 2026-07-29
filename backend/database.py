938b17ebb5a4ee86554d4227d43b4f185882302a
Krishna Pandey <krishnapandey1296@gmail.com>
2026-07-29 22:29:56 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Update the password/host/db name to match your local PostgreSQL setup
-DATABASE_URL = "postgresql://neondb_owner:npg_adyNWGgQ3TY9@ep-lingering-waterfall-ayc6dyy8-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DATABASE_URL = "postgresql+psycopg://neondb_owner:npg_adyNWGgQ3TY9@ep-lingering-waterfall-ayc6dyy8-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 # DATABASE_URL = "postgresql://postgres:harshvardhan@localhost:5432/inventory_db"
 
 engine = create_engine(DATABASE_URL)
